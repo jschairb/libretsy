64d4cde4f6837eab6f17a0594fd42121905e89ba
Josh Schairbaum <joshua.schairbaum@gmail.com>
2010-09-22 10:38:50 -0500

    Adding authenticator logic

--- lib/libretsy/authenticator.rb
+++ lib/libretsy/authenticator.rb
@@ -1,11 +1,14 @@
+require 'digest/md5'
+
 module Libretsy
   class Authenticator
 
-    attr_accessor :client, :response
+    attr_accessor :authorization_response_hash, :client, :nonce, :opaque, :qop, :realm, :response
 
     def initialize(client, response)
       @client = client
       @response = response
+      parse_response_headers(response)
     end
 
     def self.authenticate(client,response)
@@ -13,9 +16,53 @@ module Libretsy
     end
 
     def authentication_headers
+      "HERE"
+    end
+
+    def authorization_header
+      header = ""
+      header << "Digest username=\"#{client.username}\", "
+      header << "realm=\"#{realm}\", "
+
+      { "WWW-Authenticate" => header }
     end
 
     def cnonce
+      Digest::MD5.hexdigest("#{client.username}:#{client.password}:#{request_id}:#{nonce}")
+    end
+
+    def request_id
+      Digest::MD5.hexdigest(Time.now.to_f.to_s)
+    end
+
+    protected
+    def calculate_digest
+      ha1 = Digest::MD5.hexdigest("#{client.username}:#{realm}:#{client.password}")
+      ha2 = Digest::MD5.hexdigest("#{request.method}:#{request.uri}")
+      qop ? digest_with_cnonce(ha1,ha2) : digest_without_cnonce(ha1,ha2)
+    end
+
+    def digest_with_cnonce(ha1,ha2)
+      digest = "#{ha1}:#{nonce}:#{('%08x' % nc)}:#{cnonce}:#{qop}:#{ha2}"
+      Digest::MD5.hexdigest(digest)
+    end
+
+    def digest_without_cnonce(ha1, ha2)
+      digest = "#{ha1}:#{nonce}:#{ha2}"
+      Digest::MD5.hexdigest(digest)
+    end
+
+    def parse_response_headers(response)
+      parse_authorization_response_header(response.headers_hash["WWW-Authenticate"])
+      self.nonce  = authorization_response_hash["nonce"]
+      self.opaque = authorization_response_hash["opaque"]
+      self.qop    = authorization_response_hash["qop"]
+      self.realm  = authorization_response_hash["realm"]
+    end
+
+    def parse_authorization_response_header(header)
+      self.authorization_response_hash = header.gsub(/Digest |\"/, "").split(", ").
+        inject({}){|headers, header| k,v = *header.split("="); headers.merge(k=>v) }
     end
   end
 end
--- spec/libretsy/authenticator_spec.rb
+++ spec/libretsy/authenticator_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe Libretsy::Authenticator do
   before(:each) do
     @client = Libretsy::Client.new(default_client_attributes)
-    @response = { }
+    @response = mock("response",
+       :headers_hash => { "WWW-Authenticate" => "Digest realm=\"DTON\", nonce=\"e1fa8b7c8ab2f049f5db095f814d352a\", opaque=\"20d1ccc139e99b834857cae3bae672ca\", qop=\"auth\"" }
+    )
   end
 
   it "requires a client and a response as arguments" do
@@ -36,5 +38,41 @@ describe Libretsy::Authenticator do
     end
   end
 
+  describe "#authorization_header" do
+    it "returns a hash" do
+    end
+  end
+
+  describe "#cnonce" do
+  end
+
+  describe "#parse_response_headers" do
+    it "sets the nonce from the response" do
+      authenticator = Libretsy::Authenticator.new(@client, @response)
+      authenticator.nonce.should_not be_nil
+      authenticator.nonce.should == "e1fa8b7c8ab2f049f5db095f814d352a"
+    end
+
+    it "sets the realm from the response" do
+      authenticator = Libretsy::Authenticator.new(@client, @response)
+      authenticator.realm.should_not be_nil
+      authenticator.realm.should == "DTON"
+    end
+
+    it "sets the qop from the response" do
+      authenticator = Libretsy::Authenticator.new(@client, @response)
+      authenticator.qop.should_not be_nil
+      authenticator.qop.should == "auth"
+    end
+
+    it "sets the opaque from the response" do
+      authenticator = Libretsy::Authenticator.new(@client, @response)
+      authenticator.opaque.should_not be_nil
+      authenticator.opaque.should == "20d1ccc139e99b834857cae3bae672ca"
+    end
+  end
+
+  describe "#request_id" do
+  end
 
 end
