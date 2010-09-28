27e1d0ddeef848c647b76972b3200bcbf21b924d
Josh Schairbaum <joshua.schairbaum@gmail.com>
2010-09-27 20:16:06 -0500

    copying response urls to the session

--- lib/libretsy/session.rb
+++ lib/libretsy/session.rb
@@ -1,7 +1,7 @@
 module Libretsy
   class Session
 
-    attr_accessor :authorization_headers, :client, :cookie, :nonce_count, :response
+    attr_accessor :authorization_headers, :client, :cookie, :nonce_count, :response, :server_info, :urls
 
     def initialize(client)
       @authorization_headers = {}
@@ -25,39 +25,24 @@ module Libretsy
     protected
     def parse_authentication_login_response
       self.cookie = @response.headers_hash["Set-Cookie"]
+      set_urls
+    end
+
+    def select_value(key)
+      @response.body.split("\r\n").select{|s| s.include?(key)}.first.split("=")[1]
     end
 
     def set_authorization_headers
       self.authorization_headers = Authenticator.authenticate(client,response)
     end
+
+    def set_urls
+      self.urls = { :get_metadata => select_value("GetMetadata"),
+                    :get_object   => select_value("GetObject"),
+                    :login        => select_value("Login"),
+                    :logout       => select_value("Logout"),
+                    :search       => select_value("Search") }
+    end
   end
 end
 
-# Successful Headers
-# HTTP/1.1 200 OK
-# Date: Fri, 24 Sep 2010 19:36:16 GMT
-# Server: Microsoft-IIS/6.0
-# X-Powered-By: ASP.NET (L004)
-# X-AspNet-Version: 2.0.50727
-# Set-Cookie: RETS-Session-ID=038f427b60f14e13beb46822e003237a; path=/
-# RETS-Version: RETS/1.5
-# Transfer-Encoding: chunked
-# Cache-Control: private
-# Content-Type: text/xml; charset=utf-8
-
-# SUCCESSFUL RESPONSE BODY
-# <RETS ReplyCode="0" ReplyText="Operation successful." >
-# <RETS-RESPONSE>
-# MemberName=Joshua D. Schairbaum
-# User=JDSRETS109,NULL,NULL,990000195
-# Broker=999000998
-# MetadataVersion=01.50.91625
-# MinMetadataVersion=01.50.91625
-# TimeoutSeconds=1800
-# GetObject=/Dayton/DTON/getobject.aspx
-# Login=/Dayton/DTON/login.aspx
-# Logout=/Dayton/DTON/logout.aspx
-# Search=/Dayton/DTON/search.aspx
-# GetMetadata=/Dayton/DTON/getmetadata.aspx
-# </RETS-RESPONSE>
-# </RETS>
--- spec/libretsy/session_spec.rb
+++ spec/libretsy/session_spec.rb
@@ -25,12 +25,12 @@ describe Libretsy::Session do
       @unauthenticated_response = Typhoeus::Response.new(
         :code => 401, :body => "", :time => 0.1,
         :request => mock("request", :url => "http://example.com/rets/login.aspx", :host => "http://example.com", :method => :post),
-        :headers => "HTTP/1.1 401 Unauthorized. Not Authenticated....\r\nServer: Microsoft-IIS/6.0\r\nX-AspNet-Version: 2.0.50727\r\nWWW-Authenticate: Digest realm=\"DTON\", nonce=\"94a14d9b9c246e41ade3d3245570208e\", opaque=\"4de82e6b3f767a86305a570dbfbb7089\", qop=\"auth\"\r\nCache-Control: private\r\nContent-Type: text/xml\r\nDate: Sat, 25 Sep 2010 01:25:56 GMT\r\nRETS-Version: RETS/1.5\r\nTransfer-Encoding: chunked\r\nConnection: Keep-Alive\r\nX-Powered-By: ASP.NET (L002)\r\n"
+        :headers => "HTTP/1.1 401 Unauthorized. Not Authenticated....\r\nServer: Microsoft-IIS/6.0\r\nX-AspNet-Version: 2.0.50727\r\nWWW-Authenticate: Digest realm=\"RETS\", nonce=\"94a14d9b9c246e41ade3d3245570208e\", opaque=\"4de82e6b3f767a86305a570dbfbb7089\", qop=\"auth\"\r\nCache-Control: private\r\nContent-Type: text/xml\r\nDate: Sat, 25 Sep 2010 01:25:56 GMT\r\nRETS-Version: RETS/1.5\r\nTransfer-Encoding: chunked\r\nConnection: Keep-Alive\r\nX-Powered-By: ASP.NET (L002)\r\n"
       )
       @authenticated_response   = Typhoeus::Response.new(
         :code => 200, :time => 0.1,
         :headers => "HTTP/1.1 200 OK\r\nDate: Sat, 25 Sep 2010 01:25:56 GMT\r\nServer: Microsoft-IIS/6.0\r\nX-Powered-By: ASP.NET (L004)\r\nX-AspNet-Version: 2.0.50727\r\nSet-Cookie: RETS-Session-ID=9559c89451d14069b5d66550ab3c7754; path=/\r\nRETS-Version: RETS/1.5\r\nTransfer-Encoding: chunked\r\nCache-Control: private\r\nContent-Type: text/xml; charset=utf-8\r\n\r\n",
-        :body    => "<RETS ReplyCode=\"0\" ReplyText=\"Operation successful.\" >\r\n<RETS-RESPONSE>\r\nMemberName=Joshua D. Schairbaum\r\nUser=JDSRETS109,NULL,NULL,990000195\r\nBroker=999000998\r\nMetadataVersion=01.50.91625\r\nMinMetadataVersion=01.50.91625\r\nTimeoutSeconds=1800\r\nGetObject=/Dayton/DTON/getobject.aspx\r\nLogin=/Dayton/DTON/login.aspx\r\nLogout=/Dayton/DTON/logout.aspx\r\nSearch=/Dayton/DTON/search.aspx\r\nGetMetadata=/Dayton/DTON/getmetadata.aspx\r\n</RETS-RESPONSE>\r\n</RETS>\r\n"
+        :body    => "<RETS ReplyCode=\"0\" ReplyText=\"Operation successful.\" >\r\n<RETS-RESPONSE>\r\nMemberName=William H. Bonney\r\nUser=WHBRETS109,NULL,NULL,55378008\r\nBroker=55378008\r\nMetadataVersion=01.50.91625\r\nMinMetadataVersion=01.50.91625\r\nTimeoutSeconds=1800\r\nGetObject=/RETS/getobject.aspx\r\nLogin=/RETS/login.aspx\r\nLogout=/RETS/logout.aspx\r\nSearch=/RETS/search.aspx\r\nGetMetadata=/RETS/getmetadata.aspx\r\n</RETS-RESPONSE>\r\n</RETS>\r\n"
       )
     end
 
